48690f8c26506a16e6d290a3c907f8a642ea6b2d
ADIAN <45629684+AD1AN@users.noreply.github.com>
2024-04-04 18:26:22 +0300

    converted: Enums&Structs

---
+++ Source/SkyCraft/Enums/Casta.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "Casta.generated.h"
+
+UENUM(BlueprintType)
+enum class ECasta : uint8
+{
+	Archon,
+	Denizen,
+	Estray
+};
---
+++ Source/SkyCraft/Structs/Uint8MinMax.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "Uint8MinMax.generated.h"
+
+USTRUCT(BlueprintType)
+struct FUint8MinMax
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	uint8 Min = 0;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	uint8 Max = 0;
+};
